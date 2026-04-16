56ab38d5e342f524ccbb0b815c1b834057a5dd3f
kikielbe <kikielbe@gmail.com>
2026-04-16 13:09:56 +0700

    Revisi tampilan mobile 2

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'zk-hydrator-v1';
+const CACHE_NAME = 'zk-vault-mobile-v1';
 const assets = [
   './',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@11'
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700&display=swap'
 ];
 
 self.addEventListener('install', e => {
